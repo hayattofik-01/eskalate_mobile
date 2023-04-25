761c17ab3961234a5844ef191eb9b2be0726051d
hayattofik-01 <hayattofik22@gmail.com>
2023-04-25 22:28:47 +0300

    add profile page skeleton

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:profile_page/blueCard.dart';
+import 'package:profile_page/post_display.dart';
 
 
 void main() {
@@ -66,66 +68,72 @@ class _ProfilePageState extends State<ProfilePage> {
           ],
         ),
         Padding(
-          padding: const EdgeInsets.fromLTRB(60.0, 20.0, 60.0, 80.0),
-          child: Card(
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30)),
-              child: Padding(
-                  padding: EdgeInsets.all(15.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(15.0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            Container(
-                              height: 90,
-                              width: 90,
-                              decoration: BoxDecoration(
-                                border: Border.all(
-                                    //<-- SEE HERE
-                                    width: 5,
-                                    color: Color.fromARGB(255, 39, 176, 169)),
-                                borderRadius: BorderRadius.circular(30),
-                              ),
-                              child: Padding(
-                                padding: const EdgeInsets.all(7.0),
-                                child: ClipRRect(
-                                  borderRadius: BorderRadius.circular(20),
-                                  child: Image.network(
-                                      "https://media.istockphoto.com/photos/smiling-indian-man-looking-at-camera-picture-id1270067126?b=1&k=20&m=1270067126&s=612x612&w=0&h=tcabRaVlA0bsZhWCDBXxC1IYuGnh7_VuramO-vJ5jRs=",
-                                      fit: BoxFit.cover),
+          padding: const EdgeInsets.fromLTRB(60.0, 20.0, 60.0, 50.0),
+          child: Stack(
+            clipBehavior: Clip.none,
+            children: [Card(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30)),
+                child: Padding(
+                    padding: EdgeInsets.all(15.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(15.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Container(
+                                height: 90,
+                                width: 90,
+                                decoration: BoxDecoration(
+                                  border: Border.all(
+                                      //<-- SEE HERE
+                                      width: 5,
+                                      color: Color.fromARGB(255, 39, 176, 169)),
+                                  borderRadius: BorderRadius.circular(30),
+                                ),
+                                child: Padding(
+                                  padding: const EdgeInsets.all(7.0),
+                                  child: ClipRRect(
+                                    borderRadius: BorderRadius.circular(20),
+                                    child: Image.network(
+                                        "https://media.istockphoto.com/photos/smiling-indian-man-looking-at-camera-picture-id1270067126?b=1&k=20&m=1270067126&s=612x612&w=0&h=tcabRaVlA0bsZhWCDBXxC1IYuGnh7_VuramO-vJ5jRs=",
+                                        fit: BoxFit.cover),
+                                  ),
                                 ),
                               ),
-                            ),
-                            SizedBox(width: 25),
-                            Column(
-                              children: [
-                                Text("@Joviadan"),
-                                SizedBox(height: 5),
-                                Text("Jov Daniel"),
-                                SizedBox(height: 10),
-                                Text("Ux Designer"),
-                              ],
-                            )
-                          ],
+                              SizedBox(width: 25),
+                              Column(
+                                children: [
+                                  Text("@Joviadan"),
+                                  SizedBox(height: 5),
+                                  Text("Jov Daniel"),
+                                  SizedBox(height: 10),
+                                  Text("Ux Designer"),
+                                ],
+                              )
+                            ],
+                          ),
                         ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(20, 15, 0, 0),
-                        child: Text("About me"),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(20.0, 15, 20, 60),
-                        child: Text('This is a paragraph of text. '
-                            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. '
-                            'Suspendisse potenti. Aenean dapibus sapien maximus, venenatis velit non, eleifend massa. '),
-                      ),
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(20, 15, 0, 0),
+                          child: Text("About me"),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(20.0, 15, 20, 60),
+                          child: Text('This is a paragraph of text. '
+                              'Lorem ipsum dolor sit amet, consectetur adipiscing elit. '
+                              'Suspendisse potenti. Aenean dapibus sapien maximus, venenatis velit non, eleifend massa. '),
+                        ),
+                      ],
+                    ))),
+                    BlueCard()
                     ],
-                  ))),
+          ),
         ),
+        PostDisplay()
 //        Container(
 //   decoration: BoxDecoration(
 //     color: Colors.blueGrey,
